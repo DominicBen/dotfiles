e678e9747fa3a2063306c65f2f7d558ac32eb855
Dominic Benintendi <bdominic00@gmail.com>
2025-03-24 15:34:49 -0400

    added nvim

---
+++ .config/nvim/lua/mappings.lua
@@ -0,0 +1,22 @@
+require "nvchad.mappings"
+
+vim.opt.clipboard = ""
+
+-- add yours here
+local map = vim.keymap.set
+map("n", ";", ":", { desc = "CMD enter command mode" })
+map("i", "jk", "<ESC>")
+
+--
+-- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Comment
+map("n", "<C-/>", "gcc", { desc = "toggle comment", remap = true })
+map("v", "<C-/>", "gc", { desc = "toggle comment", remap = true })-- Move line up (Alt+Up)
+-- Move line up (Alt+Up)
+map("n", "<M-Up>", ":m .-2<CR>==", { desc = "Move line up", remap = true })
+map("v", "<M-Up>", ":m '<-2<CR>gv=gv", { desc = "Move selection up", remap = true })
+
+-- Move line down (Alt+Down)
+map("n", "<M-Down>", ":m .+1<CR>==", { desc = "Move line down", remap = true })
+map("v", "<M-Down>", ":m '>+1<CR>gv=gv", { desc = "Move selectea down", remap = true })
