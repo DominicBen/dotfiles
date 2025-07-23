ea7c5e682969d47bfb8e14b803f0dc2a0e930784
Dominic Benintendi <bdominic00@gmail.com>
2025-07-23 14:06:49 -0400

    made inactive windows a different color

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -7,18 +7,15 @@ local M = {}
 
 M.base46 = {
   theme = "doomchad",
-
-  -- hl_override = {
-  -- 	Comment = { italic = true },
-  -- 	["@comment"] = { italic = true },
-  -- },
+  hl_add = require("highlights").add,
+  hl_override = require("highlights").override,
 }
 
 -- M.nvdash = { load_on_startup = true }
--- M.ui = {
---       tabufline = {
---          lazyload = false
---      }
---}
+M.ui = {
+  tabufline = {
+    lazyload = false,
+  },
+}
 
 return M
---
+++ .config/nvim/lua/highlights.lua
@@ -0,0 +1,14 @@
+local M = {}
+
+M.override = {
+  Normal = {
+    bg = "black",
+  },
+}
+M.add = {
+  NormalNC = {
+    bg = { "black", 2 },
+  },
+}
+
+return M
